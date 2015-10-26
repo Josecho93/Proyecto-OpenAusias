6a8b35791f149519948d785ccf836989cc3bca9f
a044533450e <a044533450e@s09-pc04>
2015-10-26 13:25:22 +0100

    Commit de pruebas del día que empezamos cliente

--- src/main/webapp/js/specific/usuario/routes.js
+++ src/main/webapp/js/specific/usuario/routes.js
@@ -1,16 +1,14 @@
-/*
+/* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package net.daw.dao.specific.implementation;
 
-/**
- *
- * @author a044533450e
- */
-public class MotocicletaDao {
 
-    
-    
-}
+function fUsuarioRoutes() {
+
+        path.map("#usuario/new").to(function ()
+                $('#broth_content').empty().append('<h1> He ejecutado new </h1>')
+
+        )
+    }
