10fdfad7cea11aae422e35e9618eabafd1724cab
a044533450e <a044533450e@s09-pc04>
2015-10-27 10:11:53 +0100

    Comprobado del new el list y el view

--- src/main/webapp/js/specific/tipousuario/routes.js
+++ src/main/webapp/js/specific/tipousuario/routes.js
@@ -4,39 +4,8 @@
  * and open the template in the editor.
  */
 
-function fTipoUsuarioRoutes() {
-    var icon = '<i class="fa fa-file-text-o fa-5x"></i>';
-    var fillDocumentoPageHeader = _.partial(html.getPageHeader, icon, 'TipoUsuario', _);
-    var strClass = 'tipousuario';
-    var place = $('#broth_content');
-    
-    Path.map("#/" + strClass + "/new(/:url)").to(function () {
-        $('#broth_panel_heading').empty().append(fillDocumentoPageHeader('New'));
-        var strParam = parameter.getUrlObjectFromUrlString(this.params['url']);
-        ausiasFLOW.reset();
-        ausiasFLOW.initialize(newModule, place, strClass, 'new', strParam);
-        return false;
-    });
-     Path.map("#/" + strClass + "/list(/:url)").to(function () {
-        $('#broth_panel_heading').empty().append(fillDocumentoPageHeader('List'));
-        var strParam = parameter.getUrlObjectFromUrlString(this.params['url']);
-        ausiasFLOW.reset();
-        ausiasFLOW.initialize(listModule, place, strClass, 'list', strParam);
-        return false;
-    });
-    
-    Path.map("#/" + strClass + "/view(/:url)").to(function () {
-        $('#broth_panel_heading').empty().append(fillDocumentoPageHeader('View'));
-        var strParam = parameter.getUrlObjectFromUrlString(this.params['url']);
-        ausiasFLOW.reset();
-        ausiasFLOW.initialize(viewModule, place, strClass, 'view', strParam);
-        return false;
-    });
-}
- 
-
-function fTipoUsuarioRoutes() {
-    var icon = '<i class="fa fa-file-text-o fa-5x"></i>';
+ function fTipoUsuarioRoutes() {
+    var icon = '<i class="fa fa-user-secret fa-5x"></i>';
     var fillDocumentoPageHeader = _.partial(html.getPageHeader, icon, 'Tipo Usuario', _);
     var strClass = 'tipousuario';
     var place = $('#broth_content');
