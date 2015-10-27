2ef72dbadf01956a2f8629f4e16cfb3f241d3b47
a044533450e <a044533450e@s09-pc04>
2015-10-27 08:29:52 +0100

    Commit de pruebas del día que empezamos cliente

--- src/main/webapp/js/specific/usuario/routes.js
+++ src/main/webapp/js/specific/usuario/routes.js
@@ -4,11 +4,41 @@
  * and open the template in the editor.
  */
 
-
 function fUsuarioRoutes() {
-
-        path.map("#usuario/new").to(function ()
-                $('#broth_content').empty().append('<h1> He ejecutado new </h1>')
-
-        )
-    }
+    Path.map("#/usuario/new(/:url)").to(function () {
+       
+        $('#broth_panel_heading').empty().append('<h1>new de usuario ejec</h1>');
+        ausiasFLOW.reset();
+       
+        ausiasFLOW.newModule_frontOperation="new";
+        ausiasFLOW.newModule_class="usuario";
+        ausiasFLOW.initialize(newModule, $('#broth_content'));
+       
+         });
+   
+    Path.map("#/usuario/view(/:url)").to(function () {
+        $('#broth_panel_heading').empty().append('<h1>view de usuario ejec</h1>');
+        ausiasFLOW.reset();
+        ausiasFLOW.viewModule_paramsObject=parameter.getUrlObjectFromUrlString(this.params["url"]);
+        ausiasFLOW.viewModule_frontOperation="view";
+        ausiasFLOW.viewModule_class="usuario";
+        ausiasFLOW.initialize(viewModule, $('#broth_content'));
+       
+       
+    });
+    Path.map("#/usuario/delete").to(function () {
+        $('#broth_content').empty().append('<h1>he ejecutado delete</h1>');
+    });
+   
+   
+    Path.map("#/usuario/list").to(function () {
+       
+        $('#broth_panel_heading').empty().append('<h1>list de usuario ejec</h1>');
+        ausiasFLOW.reset();
+        ausiasFLOW.listModule_paramsObject=parameter.getUrlObjectFromUrlString(this.params["url"]);
+        ausiasFLOW.listModule_frontOperation="new";
+        ausiasFLOW.listModule_class="usuario";
+        ausiasFLOW.initialize(listModule, $('#broth_content'));
+       
+         });
+}
