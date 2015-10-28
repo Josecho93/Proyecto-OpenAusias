f9b96d8965a0fce77217da709b204da01695cb4b
a044533450e <a044533450e@s09-pc04>
2015-10-28 11:50:21 +0100

    Formulario de usuario

--- src/main/webapp/js/specific/usuario/usuarioNew.js
+++ src/main/webapp/js/specific/usuario/usuarioNew.js
@@ -10,26 +10,31 @@ var usuarioNew = function () {
 };
 usuarioNew.prototype = new newModule();
 usuarioNew.prototype.doEventsLoading = function () {
-    $("#usuarioForm #obj_tipousuario_button").unbind("click");
-    $("#usuarioForm #obj_tipousuario_button").click(function () {
-
-        $("#usuarioForm").append(modal.getEmptyModal());
-        modal.loadModal('#modal01', modal.getModalHeader('Elección del tipo de usuario'),
-                "", modal.getModalFooter(), true);
-
-
-//ausiasFLOW.initialize(viewModule, $('#modal-body'), 'tipousuario', 'view', {'id':3});
-
-        ausiasFLOW.initialize(ebpListModule, $('#modal-body'), 'tipousuario', 'ebplist', {}, function (id) {
-            $('#obj_tipousuario').val(id);
-            promise.getOne("tipousuario",id).done(function(json) {
-            $('#obj_tipousuario_desc').html(
-                    html.printObject2('usuario',json.message));    
-            });
-            $('#modal01').modal('hide');
-        });
-
-//$('#modal-body').append('<h2> Aquí se van a mostrar los tipos de usuario</h2>')
-
-    });
+   
+   form.getForeign("usuario","tipousuario");
+   
+   
+   
+//    $("#usuarioForm #obj_tipousuario_button").unbind("click");
+//    $("#usuarioForm #obj_tipousuario_button").click(function () {
+//
+//        $("#usuarioForm").append(modal.getEmptyModal());
+//        modal.loadModal('#modal01', modal.getModalHeader('Elección del tipo de usuario'),
+//                "", modal.getModalFooter(), true);
+//
+//
+////ausiasFLOW.initialize(viewModule, $('#modal-body'), 'tipousuario', 'view', {'id':3});
+//
+//        ausiasFLOW.initialize(ebpListModule, $('#modal-body'), 'tipousuario', 'ebplist', {}, function (id) {
+//            $('#obj_tipousuario').val(id);
+//            promise.getOne("tipousuario",id).done(function(json) {
+//            $('#obj_tipousuario_desc').html(
+//                    html.printObject2('usuario',json.message));    
+//            });
+//            $('#modal01').modal('hide');
+//        });
+//
+////$('#modal-body').append('<h2> Aquí se van a mostrar los tipos de usuario</h2>')
+//
+//    });
 };
